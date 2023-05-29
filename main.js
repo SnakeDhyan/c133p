78e741e55a5c7a2ee621ac886a1e52e4ef6a87ba
SnakeDhyan <98876616+SnakeDhyan@users.noreply.github.com>
2023-05-29 13:56:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function bed(){
+    window.location="bed.html"
+}
+function des(){
+    window.location="des.html"
+}
+function tvac(){
+    window.location="tvac.html"
+}
+function bot(){
+    window.location="bot.html"
+}
+function ft(){
+    window.location="ft.html"
+}
